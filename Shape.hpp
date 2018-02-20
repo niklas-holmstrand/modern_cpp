e767d65ec26404b2b98718534cc5f15dc68b39e7
Niklas <niklas.holmstrand@gmail.com>
2018-02-20 11:50:20 +0100

    shape has color

--- Shape.hpp
+++ Shape.hpp
@@ -1,11 +1,21 @@
 #pragma once
+enum class Color{
+    Red,
+    Green,
+    Blue
+};
+
 
 class Shape
 {
+private:
+    Color c_ = Color::Blue;
+
 public:
     virtual ~Shape() {}
 
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
     virtual void print() const;
+    virtual Color getColor() { return c_;};
 };
